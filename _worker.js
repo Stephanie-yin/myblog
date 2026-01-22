e5598d03caaca7d5a590a5e84142d8d8632ebb30
Stephanie-yin <yinchangli0225@163.com>
2026-01-22 19:56:57 +0800

    Update UUID 0122-1956

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '8a85227d-ec9c-4a4f-9e40-292a13fe489d';
+let userID = 'a5e57d1e-d7ac-4c25-8aa0-3703da55a20e';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
