6fc741861b186354394a1918ec404e117049efeb
Stephanie-yin <yinchangli0225@163.com>
2026-01-23 08:45:14 +0800

    Fix proxy IP

--- _worker.js
+++ _worker.js
@@ -6,10 +6,10 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'b9ebd096-8c18-4f70-a539-d7be2faf9993';
 
 // 生成配置文件的 CF 优选 IP
-const bestCFIP = "www.gov.se"
+const bestCFIP = "myblog-13y.pages.dev"
 
 // 用于 CF 网站的代理 IP
-const proxyIPs = ["workers.cloudflare.cyou"]; // const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ["myblog-13y.pages.dev"];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
